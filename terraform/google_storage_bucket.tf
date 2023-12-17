c15204828bf03e362406b31eb3d8ec657532df78
Dziuba Maksym <dziubamaksym@pm.me>
2023-12-17 21:15:05 +0200

    init tf code

---
+++ terraform/google_storage_bucket.tf
@@ -0,0 +1,14 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "hacker_news_bucket" {
+  name          = "hacker-news-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket_object" "hacker_news_code" {
+  name   = "main.py"
+  source = "../main.py"
+  bucket = google_storage_bucket.hacker_news_bucket.name
+}
