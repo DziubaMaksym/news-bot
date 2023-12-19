8dd8223772aaf42878b7d5ba255990ebaab1ea2c
Dziuba Maksym <dziubamaksym@pm.me>
2023-12-19 21:21:23 +0200

    fix vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,22 @@
 variable "project" {
   description = "Project ID"
   type        = string
+  sensitive   = true
 }
 
 variable "region" {
   description = "Region of GCP"
   type        = string
+  sensitive   = true
 }
 
 variable "mastodon_access_token" {
   description = ""
   type        = string
+  sensitive   = true
 }
 variable "mastodon_api_base_url" {
   description = ""
   type        = string
+  sensitive   = true
 }
