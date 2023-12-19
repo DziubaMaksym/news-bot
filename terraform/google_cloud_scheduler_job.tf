5df0ded9c2a9910bef7d3cf357876e58a76ef957
Dziuba Maksym <dziubamaksym@pm.me>
2023-12-19 19:51:54 +0200

    fix schedule

--- terraform/google_cloud_scheduler_job.tf
+++ terraform/google_cloud_scheduler_job.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_scheduler_job" "cloud_function_trigger" {
   name        = "hacker-news-function-trigger"
   description = "Trigger Hacker News function every 20 minutes"
-  schedule    = "*/25 * * * *"
+  schedule    = "*/20 * * * *"
   time_zone   = "Etc/UTC"
 
   http_target {
