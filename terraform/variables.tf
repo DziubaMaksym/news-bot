0563a45b01098617997907bfcb03ffa8585411d5
Dziuba Maksym <19823521+DziubaMaksym@users.noreply.github.com>
2023-12-19 09:01:42 +0200

    Dev (#7)
    * add bucket upload and zip creation
    
    * remove manual upload to gcs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,12 @@ variable "region" {
   description = "Region of GCP"
   type        = string
 }
+
+variable "mastodon_access_token" {
+  description = ""
+  type        = string
+}
+variable "mastodon_api_base_url" {
+  description = ""
+  type        = string
+}
